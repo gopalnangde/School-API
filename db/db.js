05b779ef3522389e60d7b55633fde93699eca5e1
gopalnangde <gopalnangde5@gmail.com>
2026-05-14 21:38:03 +0530

    Set environmental variables

--- db/db.js
+++ db/db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config()
 
   const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: "college",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
 
   db.connect((err) => {
